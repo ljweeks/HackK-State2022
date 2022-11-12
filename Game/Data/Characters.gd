259a7e8fc12aae61c4a7ff2eafa0ed461e2e5799
Ethan Tucker <ectucker1@gmail.com>
2022-11-12 14:09:42 -0600

    Add character data structure

--- Game/Data/Characters.gd
+++ Game/Data/Characters.gd
@@ -4,16 +4,11 @@ extends Node
 var CHARACTERS = []
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func add_character(character):
+	CHARACTERS.append(character)
 
-func add_character(char_data):
-	print("character added")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
 func get_character(char_name):
 	for item in CHARACTERS:
-		if(item.name == char_name):
+		if item.name == char_name:
 			return item
 
