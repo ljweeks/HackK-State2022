4781a011e6f64b9acac4daec9fa6380f28820dda
lexweeks <lexweeks@gmail.com>
2022-11-12 16:56:59 -0600

    fixed rerecord

--- Game/Data/Character.gd
+++ Game/Data/Character.gd
@@ -1,6 +1,8 @@
 class_name Character
 extends Resource
 
+enum {BLOCK, MOVE_LEFT, MOVE_RIGHT, MOVE1, MOVE2, MOVE3, MOVE4, NO_MOVE}
+
 export var move_frames = {}
 
 export var move_ranges = {}
@@ -43,3 +45,37 @@ func _harmless_frame():
 	}
 	
 #HURT BOX GENERATION
+func get_hurt_boxes():
+	hurt_box_for_move(MOVE1)
+	hurt_box_for_move(MOVE2)
+	hurt_box_for_move(MOVE3)
+	hurt_box_for_move(MOVE4)	
+	
+func hurt_box_for_move(move):
+	var largest = 0
+	var largest_box
+	var frame_number
+	var index = 0
+	for frame in move_frames[move]:
+		if(abs(frame["hurtboxes"]["left_hand"]["x"])-320 > largest):
+			largest = abs(frame["hurtboxes"]["left_hand"]["x"])-320
+			largest_box = "left_hand"
+			frame_number = index
+		if(abs(frame["hurtboxes"]["right_hand"]["x"])- 320 > largest):
+			largest = abs(frame["hurtboxes"]["right_hand"]["x"])- 320
+			largest_box = "right_hand"
+			frame_number = index
+		if(abs(frame["hurtboxes"]["left_foot"]["x"])- 320 > largest):
+			largest = abs(frame["hurtboxes"]["left_foot"]["x"]) -320
+			largest_box = "left_foot"
+			frame_number = index
+		if(abs(frame["hurtboxes"]["right_foot"]["x"]) - 320 > largest):
+			largest = abs(frame["hurtboxes"]["right_foot"]["x"]) -320
+			largest_box = "right_foot"
+			frame_number = index
+		index += 1
+		if(index > move_ranges[move]["stop"]):
+			break
+	move_frames[move][frame_number-1]["hurtboxes"][largest_box]["active"] = true
+	print("move-" + str(move) + " frame-" + str(frame_number-1) + " for-" + largest_box)
+	
--- Game/Menus/create_character_menu.gd
+++ Game/Menus/create_character_menu.gd
@@ -3,7 +3,7 @@ extends Control
 enum {BLOCK, MOVE_LEFT, MOVE_RIGHT, MOVE1, MOVE2, MOVE3, MOVE4, NO_MOVE}
 
 onready var cur_selected = BLOCK
-var all_checked = {}
+
 onready var slider = get_node("VBoxContainer/HBoxContainer2/frame_select")
 onready var start_frame = get_node("VBoxContainer/HBoxContainer/start") 
 onready var stop_frame = get_node("VBoxContainer/HBoxContainer/stop")
@@ -14,7 +14,7 @@ var moves_frames = {}
 var moves_images = {}
 var frame_data = []
 var image_data = []
-
+var all_checked = {BLOCK : false, MOVE_LEFT : false, MOVE_RIGHT : false, MOVE1 : false, MOVE2 : false, MOVE3 : false, MOVE4 : false}
 var can_save
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,13 +23,13 @@ func _ready():
 	start_frame.min_value = 0
 	stop_frame.min_value = 1
 	$VBoxContainer2/save.modulate = Color.transparent
-	all_checked.BLOCK = false
-	all_checked.MOVE_LEFT = false
-	all_checked.MOVE_RIGHT = false
-	all_checked.MOVE1 = false
-	all_checked.MOVE2 = false
-	all_checked.MOVE3 = false
-	all_checked.MOVE4 = false
+#	all_checked.BLOCK = false
+#	all_checked.MOVE_LEFT = false
+#	all_checked.MOVE_RIGHT = false
+#	all_checked.MOVE1 = false
+#	all_checked.MOVE2 = false
+#	all_checked.MOVE3 = false
+#	all_checked.MOVE4 = false
 	$VBoxContainer2/block.connect("button_clicked", self, "_on_move_select")
 	$VBoxContainer2/left.connect("button_clicked", self, "_on_move_select")
 	$VBoxContainer2/right.connect("button_clicked", self, "_on_move_select")
@@ -49,31 +49,31 @@ func _on_record_finished(images, frames):
 	if(cur_selected == BLOCK):
 		print("recorded block")
 		$VBoxContainer2/block.complete.set_texture(load("res://Icons/checkmark.png"))
-		all_checked.BLOCK = true
+		all_checked[BLOCK] = true
 	elif(cur_selected == MOVE_LEFT):
 		print("recorded move left")
 		$VBoxContainer2/left.complete.set_texture(load("res://Icons/checkmark.png"))
-		all_checked.MOVE_LEFT = true
+		all_checked[MOVE_LEFT] = true
 	elif(cur_selected == MOVE_RIGHT):
 		print("recorded move right")
 		$VBoxContainer2/right.complete.set_texture(load("res://Icons/checkmark.png"))
-		all_checked.MOVE_RIGHT = true
+		all_checked[MOVE_RIGHT] = true
 	elif(cur_selected == MOVE1):
 		print("recorder move 1")
 		$VBoxContainer2/move1.complete.set_texture(load("res://Icons/checkmark.png"))
-		all_checked.MOVE1 = true
+		all_checked[MOVE1] = true
 	elif(cur_selected == MOVE2):
 		print("recorded move 2")
 		$VBoxContainer2/move2.complete.set_texture(load("res://Icons/checkmark.png"))
-		all_checked.MOVE2 = true
+		all_checked[MOVE2] = true
 	elif(cur_selected == MOVE3):
 		print("recorded move 3")
 		$VBoxContainer2/move3.complete.set_texture(load("res://Icons/checkmark.png"))
-		all_checked.MOVE3 = true
+		all_checked[MOVE3] = true
 	elif(cur_selected == MOVE4):
 		print("recorded move 4")
 		$VBoxContainer2/move4.complete.set_texture(load("res://Icons/checkmark.png"))
-		all_checked.MOVE4 = true
+		all_checked[MOVE4] = true
 	var val = all_checked.values()
 	for item in val:
 		if(not item):
@@ -96,6 +96,7 @@ func _on_move_select(type):
 func _on_record_toggled(button_pressed):
 	if(button_pressed): #if the button is down we want to start recording
 		print("Start recording")
+		all_checked[cur_selected] = false
 		if(not GlobalCameraServer.server_recording):
 			GlobalCameraServer.start_record()
 	else:
@@ -104,19 +105,19 @@ func _on_record_toggled(button_pressed):
 
 
 func _process(delta):
-	if(cur_selected == BLOCK and all_checked.BLOCK == true):
+	if(cur_selected == BLOCK and all_checked[BLOCK] == true):
 		display_preview(BLOCK)
-	elif(cur_selected == MOVE_LEFT and all_checked.MOVE_LEFT == true):
+	elif(cur_selected == MOVE_LEFT and all_checked[MOVE_LEFT] == true):
 		display_preview(MOVE_LEFT)
-	elif(cur_selected == MOVE_RIGHT and all_checked.MOVE_RIGHT == true):
+	elif(cur_selected == MOVE_RIGHT and all_checked[MOVE_RIGHT] == true):
 		display_preview(MOVE_RIGHT)
-	elif(cur_selected == MOVE1 and all_checked.MOVE1 == true):
+	elif(cur_selected == MOVE1 and all_checked[MOVE1] == true):
 		display_preview(MOVE1)
-	elif(cur_selected == MOVE2 and all_checked.MOVE2 == true):
+	elif(cur_selected == MOVE2 and all_checked[MOVE2] == true):
 		display_preview(MOVE2)
-	elif(cur_selected == MOVE3 and all_checked.MOVE3 == true):
+	elif(cur_selected == MOVE3 and all_checked[MOVE3] == true):
 		display_preview(MOVE3)
-	elif(cur_selected == MOVE4 and all_checked.MOVE4 == true):
+	elif(cur_selected == MOVE4 and all_checked[MOVE4] == true):
 		display_preview(MOVE4)
 	else:
 		display_preview(NO_MOVE)
@@ -140,16 +141,26 @@ func display_preview(move):
 		$VBoxContainer/frames.set_texture(moves_images[cur_selected][slider.value])
 		$VBoxContainer/HBoxContainer2/frame_number.text = str(slider.value)
 		moves_ranges[cur_selected] = {'start': start_frame.value, 'stop' : stop_frame.value}
+		var bones = moves_frames[cur_selected][slider.value]
+		if(bones.size() > 0):
+			for name in bones["character_colliders"].keys():
+				var bone = bones["character_colliders"][name]
+				var line = get_node("VBoxContainer/frames/" + name)
+				line.set_point_position(0, Vector2(bone["x1"], bone["y1"]))
+				line.set_point_position(1, Vector2(bone["x2"], bone["y2"]))
 
 
 
 
 
 func _on_TextureButton_pressed():
+	print("SAVE")
 	if start_frame.value < stop_frame.value and can_save:
 		var char_data = Character.new()
 		char_data.name = $VBoxContainer/HBoxContainer/name.text
 		char_data.move_frames = moves_frames
 		char_data.move_ranges = moves_ranges
 		char_data.move_images = moves_images
+		char_data.get_hurt_boxes()
 		Characters.add_character(char_data)
+		get_tree().change_scene("res://Menus/character_select.tscn")
