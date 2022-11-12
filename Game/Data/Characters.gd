f5ee113e5dc11d3f2e903c5b7cc30cba781aaad5
Ethan Tucker <ectucker1@gmail.com>
2022-11-12 14:26:01 -0600

    Generate some empty characters on launch

--- Game/Data/Characters.gd
+++ Game/Data/Characters.gd
@@ -4,6 +4,12 @@ extends Node
 var CHARACTERS = []
 
 
+func _ready():
+	for i in range(0, 3):
+		var char_data = Character.new()
+		char_data.name = "Empty " + str(i)
+		add_character(char_data)
+
 func add_character(character):
 	CHARACTERS.append(character)
 
