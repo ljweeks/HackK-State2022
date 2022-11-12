5e8ad0d5b9e86b571100c8d23fcd8f87473d7f3d
Ethan Tucker <ectucker1@gmail.com>
2022-11-12 16:41:20 -0600

    Update hitboxes as animation plays

--- Game/Player/animator.gd
+++ Game/Player/animator.gd
@@ -19,6 +19,20 @@ var playing := false
 onready var player = get_parent()
 onready var sprite = player.get_node("Sprite")
 
+func _ready():
+	var damage_box = sprite.get_node("damage_box")
+	var owners = damage_box.get_shape_owners()
+	for owner in owners:
+		var shape_count = damage_box.shape_owner_get_shape_count(owner)
+		var shape_owner_obj = damage_box.shape_owner_get_owner(owner)
+		var shape_obj = null
+		for shape in range(0, shape_count):
+			shape_obj = damage_box.shape_owner_get_shape(owner, shape)
+		damage_box.shape_owner_clear_shapes(owner)
+		shape_obj = shape_obj.duplicate(true)
+		damage_box.shape_owner_add_shape(owner, shape_obj)
+		shape_owner_obj.set_shape(shape_obj)
+
 func play(move) -> void:
 	anim_images = character.get_images(move)
 	anim_ranges = character.get_range(move)
@@ -36,6 +50,19 @@ func stop() -> void:
 
 func show_frame(image, frame):
 	sprite.texture = image
+	var bones = frame['character_colliders']
+	
+	var damage_box: Area2D = sprite.get_node('damage_box')
+	var owners = damage_box.get_shape_owners()
+	for owner in owners:
+		var shape_count = damage_box.shape_owner_get_shape_count(owner)
+		var shape_owner_name = damage_box.shape_owner_get_owner(owner).name
+		if shape_owner_name in bones:
+			var bone = bones[shape_owner_name]
+			for shape in range(0, shape_count):
+				var shape_obj = damage_box.shape_owner_get_shape(owner, shape)
+				shape_obj.b = Vector2(bone["x1"], bone["y1"])
+				shape_obj.a = Vector2(bone["x2"], bone["y2"])
 
 func _physics_process(delta: float) -> void:
 	if playing:
